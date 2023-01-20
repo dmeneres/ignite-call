a3df81eec0af21cb7d060eee715206d7965d4dab
Diogo Menéres <dmeneres@gmail.com>
2023-01-20 02:34:25 +0000

    home visuals

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Scheduling uncomplicated</Heading>
+        <Text size="xl">
+          Connect your calendar and let people book appointments in your free
+          time.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendar symbolizing application in operation"
+        />
+      </Preview>
+    </Container>
+  )
+}
